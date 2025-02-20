b94ac15ab7b409d9637ae0b43c93a2f86ce36baa
JungminLee97 <jungmin7618@naver.com>
2025-02-20 18:12:03 +0900

    Feat: Home페이지에 링크태그(login페이지로이동) 생성 및 AuthForm컴포넌트 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>Home</div>
+      <Link to="/login">dd</Link>
+    </>
+  );
 };
 
 export default Home;
