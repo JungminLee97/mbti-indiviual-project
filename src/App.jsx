09e7ec660e6f19fa12baa313330911543f693b8e
JungminLee97 <jungmin7618@naver.com>
2025-02-20 18:05:59 +0900

    Feat: react-router-dom설정 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+import Test from "./pages/Test";
+import TestResult from "./pages/TestResult ";
+
 function App() {
-  return <div className="bg-blue-500">dd</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/testResult" element={<TestResult />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
