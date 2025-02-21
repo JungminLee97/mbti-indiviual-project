b9584ffb81370dcb4599b28b34cb8a365e34fcef
JungminLee97 <jungmin7618@naver.com>
2025-02-21 22:29:14 +0900

    feat, fix: 로그인 됐을때, 안됐을때 강제이동시키는 페이지 기능 body: Outlet을 이용한 방법과 context API 를이용하여 원장님강의처럼하는방법 모두 구현완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Headerbar from "../components/Headerbar";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -7,15 +7,26 @@ import Signup from "../pages/Signup";
 import Test from "../pages/Test";
 import TestResult from "../pages/TestResult";
 import ProtectedRoute from "../components/ProtectedRoute";
-const RouterComponent = () => {
-  const isAuthenticated = false;
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext";
 
+// const ProtectedRouteSecondMethods  = ({ element: Element, ...rest }) => {
+//   const { isAuthenticated } = useContext(AuthContext);
+//   return isAuthenticated ? <Element {...rest} /> : <Navigate to="/login" />;
+// };
+
+const PublicRoute = ({ element: Element, ...rest }) => {
+  const { isAuthenticated } = useContext(AuthContext);
+  return !isAuthenticated ? <Element {...rest} /> : <Navigate to="/profile" />;
+};
+
+const RouterComponent = ({ isAuthenticated }) => {
   return (
     <Routes>
       <Route element={<Headerbar />}>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<PublicRoute element={Login} />} />
+        <Route path="/signup" element={<PublicRoute element={Signup} />} />
       </Route>
       <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
         <Route path="/profile" element={<Profile />} />
