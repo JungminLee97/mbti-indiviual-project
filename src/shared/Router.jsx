32f8631cc853d3d965af393a7a8832fba9800827
JungminLee97 <jungmin7618@naver.com>
2025-02-21 15:31:56 +0900

    feat: ProtectedRoute이용해서 로그인안됐으면 login페이지로 강제이동시키게하기구현완료

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -6,20 +6,30 @@ import Profile from "../pages/Profile";
 import Signup from "../pages/Signup";
 import Test from "../pages/Test";
 import TestResult from "../pages/TestResult";
+import ProtectedRoute from "../components/ProtectedRoute";
+const RouterComponent = () => {
+  const isAuthenticated = false;
+
+  return (
+    <Routes>
+      <Route element={<Headerbar />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Route>
+      <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/testResult" element={<TestResult />} />
+      </Route>
+    </Routes>
+  );
+};
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<Headerbar />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Route>
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/testResult" element={<TestResult />} />
-      </Routes>
+      <RouterComponent />
     </BrowserRouter>
   );
 };
