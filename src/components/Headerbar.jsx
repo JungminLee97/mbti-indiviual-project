e5917b8b835553e24e89c604aae729f1843e805e
JungminLee97 <jungmin7618@naver.com>
2025-02-21 15:09:19 +0900

    feat: app -> router 로 로직 이동, outlet 사용하기 body: router을 app에서 router컴포넌트로옮기고 app에 컴포넌트를 줌 uselocation으로 공통헤더바 만들것을, react-router Outlet으로 공통헤더바 만들어서사용변경함

--- src/components/Headerbar.jsx
+++ src/components/Headerbar.jsx
@@ -1,25 +1,28 @@
-import { Link } from "react-router";
+import { Link, Outlet } from "react-router";
 
 const Headerbar = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "lightgreen",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: "15px",
-        fontSize: "20px",
-        fontWeight: "bold",
-      }}
-    >
-      <Link to={"/"}>
-        <p style={{ margin: "0 20px " }}>홈 </p>
-      </Link>
-      <Link to={"/login"}>
-        <p style={{ margin: "0 20px " }}>로그인</p>
-      </Link>
-    </div>
+    <>
+      <div
+        style={{
+          backgroundColor: "lightgreen",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "15px",
+          fontSize: "20px",
+          fontWeight: "bold",
+        }}
+      >
+        <Link to={"/"}>
+          <p style={{ margin: "0 20px " }}>홈 </p>
+        </Link>
+        <Link to={"/login"}>
+          <p style={{ margin: "0 20px " }}>로그인</p>
+        </Link>
+      </div>
+      <Outlet />
+    </>
   );
 };
 
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Headerbar from "../components/Headerbar";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
@@ -7,31 +7,19 @@ import Signup from "../pages/Signup";
 import Test from "../pages/Test";
 import TestResult from "../pages/TestResult";
 
-const RouterComponent = () => {
-  const location = useLocation();
-  const checkingIncludePathName = ["/", "/login", "/signup"].includes(
-    location.pathname
-  );
-
-  return (
-    <>
-      {checkingIncludePathName && <Headerbar />}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/testResult" element={<TestResult />} />
-      </Routes>
-    </>
-  );
-};
-
 const Router = () => {
   return (
     <BrowserRouter>
-      <RouterComponent />
+      <Routes>
+        <Route element={<Headerbar />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/testResult" element={<TestResult />} />
+      </Routes>
     </BrowserRouter>
   );
 };
